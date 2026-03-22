b47f51f3a44aa25aaa168dedf0449c94eba3eccd
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-03-22 14:13:59 +0530

    anagram basics

--- numpy1.py
+++ numpy1.py
@@ -1,5 +1,12 @@
 import numpy as np
 
-arr=np.array([1,2,3,4,45,3])
+arr=np.array([[1,2,3,4,45,3]])
 new=arr*2
-print(new)
+print(arr.ndim)
+
+lis=[]
+i=1
+for i in range(1,11):
+    lis.append(i)
+
+print(lis)    
