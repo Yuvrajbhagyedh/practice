ab6645fb22519603b16d3379b6ee60df9afc255f
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-03-22 12:44:47 +0530

    just practising basics

--- fibo.py
+++ fibo.py
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-def fibo(num):
-    if num<=1:
-        return num
-    else:
-        return fibo(num-1)+fibo(num-2)
-
-for i in range(10):
-=======
-def fibo(num):
-    if num<=1:
-        return num
-    else:
-        return fibo(num-1)+fibo(num-2)
-
-for i in range(10):
->>>>>>> 4d053d23d8d11bb514940fc8e750f65a0014dc4e
-    print(fibo(i))       
+lis=[2,4,34,53,23,45,67,89,90]
+sum=0
+for i in lis:
+    sum+=i*2
+    
+print(sum)
