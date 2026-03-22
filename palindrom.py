7d747755e0aebb990a6601c65c1459101a4014c6
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-03-23 00:00:36 +0530

    palindrom with negative values

---
+++ palindrom.py
@@ -0,0 +1,14 @@
+s=-121
+
+if s<0:
+    print("its not ") 
+
+while(temp>=0):
+    digit=temp%10
+    rev=rev*10+digit
+    temp=temp//10
+
+if rev==s:
+    print("palindrome number")
+else:
+    print("not")            
