09a6713919f57e61dad77ee1689de813ae08a366
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-03-29 19:26:50 +0530

    freq

--- dicti.py
+++ dicti.py
@@ -1,3 +1,12 @@
-di={}
-di["name"]="yuvraj"
-print(di)
+lis=[1,2,3,45,6,4,23]
+target=51
+
+hashmap={}
+
+for i in range(len(lis)):
+    num=lis[i]
+    diff=target-num
+
+    if diff in hashmap:
+        print(hashmap[diff],i)
+    hashmap[num]=i    
