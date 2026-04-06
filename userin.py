5b89435a2baadbac7a4e06db5650b00d017eb01d
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-06 08:55:39 +0530

    stu

---
+++ userin.py
@@ -0,0 +1,6 @@
+name=input("enter your name :")
+age=int(input("enter your age :"))
+height=float(input("enter your height :"))
+is_student=input("are you tudent True or False answer in True or false :").lower()=="true"
+
+print(f"the name of the person is :{name} and the age of that person is {age} and his/her height is {height} and the final one is he or her is student {is_student}")
