5d7420e6fab091a409845adfa080218dcb33028c
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-06 07:47:33 +0530

    day 1

---
+++ d1.py
@@ -0,0 +1,8 @@
+print("my name is yuvraj")
+name="Yuvraj"
+age=23
+height=186.3
+is_student=True
+
+
+print(f"my name is {name} and my age is {age} my height is {height}and am i student ? {is_student}")
