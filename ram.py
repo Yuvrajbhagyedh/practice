21d7a27c639b2a863fcea7037874f442ccf624b9
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-06 10:33:19 +0530

    yess

---
+++ ram.py
@@ -0,0 +1,6 @@
+text = ""
+
+while text != "exit":
+    text = input("Enter something: ")
+
+print("Program stopped")
---
+++ whi.py
@@ -0,0 +1,15 @@
+num=10
+n=10
+for i in range(1,11):
+    if i==5 or i==7:
+        continue
+    else:
+        print(i)
+i=0
+
+while(i<=n):
+    if i==5 or i==7:
+        continue
+    print(i) 
+    i+=1
+     
