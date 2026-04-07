72d3841d76d2040af0fa6b9a99bd4ddf497e47e3
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-07 15:52:00 +0530

    init

---
+++ init.py
@@ -0,0 +1,20 @@
+class student:
+    def __init__(self,name,mark1,mark2,mark3):
+        self.name=name
+        self.mark1=mark1
+        self.mark2=mark2
+        self.mark3=mark3
+
+    def average(self):
+        a=self.mark1
+        b=self.mark2
+        c=self.mark3
+        avg=(a+b+c)/3
+
+        print(avg)
+        
+
+        
+s1=student("yuvraj",99,98,97)
+s1.average()
+       
