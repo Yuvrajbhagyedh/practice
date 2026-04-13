f2d3521fe0912dfbf3bcc5aa241edddcace90cee
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-13 18:41:51 +0530

    oops

--- abstract.py
+++ abstract.py
@@ -4,14 +4,22 @@ class car(ABC):
     @abstractmethod
     def start(self):
         pass
+    @abstractmethod
+    def ram(self):
+        pass
 
 class tesla(car):
     def start(self):
         print("car started")
+    def ram(self):
+        print("random")
 class rangerover(car):
     def start(self):
-        print("range rover started")
+        print("car started")
+    def ram(self):
+        print("random")
+   
 s1=tesla()
 s1.start()    
 s2=rangerover()
-s2.start()    
+s2.ram()    
