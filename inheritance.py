04f1da7bc8c439ad82c3d4c3e240cf3b02cc7ab3
Yuvraj K <Yuvrajbhagyedh@gmail.com>
2026-04-13 18:58:28 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+class animal:
+    def sleep(slef):
+        print("animal sleeps")
+class human(animal):
+    def eats(self):
+        print("human also eats")        
+class mamma:
+    def mm(self):
+        print("naan mammal")
+class lion(human,mamma):
+    def hunt(self):
+        print("bete ado prani naanu")        
+
+s1=lion()
+s1.sleep()
+s1.hunt()
+s1.eats()
+
+s1.mm()
